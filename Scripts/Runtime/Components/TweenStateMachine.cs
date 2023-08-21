b6d820b514f12a51f0ddf001cd46c94e322f61e2
Frank Schönhofer <frankschoenhofer@gmail.com>
2023-08-21 11:41:57 +0200

    propagate matched stateName to children

--- Scripts/Runtime/Components/TweenStateMachine.cs
+++ Scripts/Runtime/Components/TweenStateMachine.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using FullCircleTween.Attributes;
 using FullCircleTween.Core;
-using FullCircleTween.Core.Interfaces;
 using FullCircleTween.Extensions;
 using FullCircleTween.Properties;
-using UnityEditor;
 using UnityEngine;
 
 namespace FullCircleTween.Components
